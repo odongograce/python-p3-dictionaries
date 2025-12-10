751cb37890506769d5344357f06a35a962f308fd
Grace Odongo <graceakinyi024@gmail.com>
2025-12-10 10:17:56 +0300

    updates second.py file

--- second_app.py
+++ second_app.py
@@ -1,11 +1,20 @@
 my_dict = {
-    "key_one": "value one",
-    "key_two": "value two",
+    "key_one": "value one"
 }
-#using dict
-# my_dict.update({"key_one":"I've changed", "key_three":"value three"})
-#using list
-# my_dict.update({"key_one":"I've changed", "key_three":"value three"})
-my_dict.update([["key_one","I've changed"], ["key_three","value three"]])
+
+# passing in an array of arrays
+my_dict.update([["key_one", "new value one"], ["key_two", "value two"]])
+
+# passing in an array of tuples
 my_dict.update([("key_two", "new value two"), ("key_three", "value three")])
+
+# passing in a tuple of arrays
+my_dict.update((["key_three", "new value three"], ["key_four", "value four"]))
+
+# passing in a tuple of tuples
+my_dict.update((("key_four", "new value four"), ("key_five", "value five")))
+
+# using assignment operation
+my_dict.update(key_five="new value five", key_six="value six")
+
 print(my_dict)
